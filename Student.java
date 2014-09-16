8763eb4c966e7487b65a5f37e0e7d89697e87794
Ben Orchard <thefirstmuffinman@gmail.com>
2014-09-16 08:49:37 +0100

    Remove Student.numOfTests
    numOfTests should not be stored in Student. If a Student is initialised, it should be given the number of tests to first initialise by the calling Database.

--- Student.java
+++ Student.java
@@ -5,23 +5,21 @@ import java.util.HashMap;
 
 public class Student {
 	private String name;
-	private int numOfTests;
 	private HashMap<Integer, Integer> results;
 	
 	public Student(String name, int numOfTests) {
 		this.name = name;
-		this.numOfTests = numOfTests;
 		results = new HashMap<Integer, Integer>();
-		this.initialise();
+		this.initialise(numOfTests);
 	}
 	
 	public String getName() {
 		return this.name;
 	}
 	
-	private void initialise() {
+	private void initialise(numOfTests) {
 		// fake/not very usefully implemented ID system atm
-		for (int testID = 0; testID < this.numOfTests; testID++) {
+		for (int testID = 0; testID < numOfTests; testID++) {
 			this.results.put(testID, -1);
 		}
 	}
