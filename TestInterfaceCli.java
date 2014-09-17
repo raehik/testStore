66164062fb9c7708b8d1817ab781fea16fc00c6f
Ben Orchard <thefirstmuffinman@gmail.com>
2014-09-17 21:56:46 +0100

    TestInterfaceCli: re-prompt for bad values
    Wrap everything in a do-while loop to continually prompt if we don't
    get what we want.
    
    Removed a bunch of old, commented-out tests in runTest(), and the
    shorter println functions (lolol).
    
    Removed addSet() too, useless & unused.
    
    General cleanup in bulkAddStudents().

--- TestInterfaceCli.java
+++ TestInterfaceCli.java
@@ -6,6 +6,55 @@ import java.util.Scanner;
 public class TestInterfaceCli {
 	public TestDatabase db;
 	
+	public void setResultsFor(int testId) {
+		int percent;
+		boolean inRange;
+		
+		// iterate over all existing students
+		for (Integer id: this.db.getAllStudentIds()) {
+			do {
+				inRange = true;
+				// prompt for percentage
+				System.out.println("Result for " + this.db.getStudentName(id) + " (ID " + id + ") (%): ");
+				try {
+					percent = new Scanner(System.in).nextInt();
+				} catch (InputMismatchException e) {
+					// if it wasn't an integer
+					System.out.println("ERROR: not an integer");
+					inRange = false;
+					continue;
+				}
+				
+				// try to set result with percent input
+				int ret = this.db.setResultOfStudent(id, testId, percent);
+				if (ret == 1) {
+					System.out.println("ERROR: not a percentage (0-100)");
+					inRange = false;
+					continue;
+				}
+			} while (! inRange); // keep trying if we had an error
+		}
+	}
+	
+	public void bulkAddStudents(int num) {
+		// Add `num` students, prompting for names & printing their ID.
+		String name;
+		int id;
+		
+		for (int i = 0; i < num; i++) {
+			// prompt for name
+			System.out.println("Student " + (i + 1) + "/" + num + " name: ");
+			
+			// TODO: make sure they enter something
+			//       (check for `\n`s etc?)
+			name = new Scanner(System.in).nextLine();
+			id = this.db.addStudent(name);
+			
+			System.out.println(name + " added (ID " + id + ")");
+		}
+		name = null;
+	}
+	
 	public void printDatabase() {
 		int result;
 		
@@ -60,24 +109,6 @@ public class TestInterfaceCli {
 		System.out.println(name + " scored " + result + "% (" + grade + ") on test \"" + test + "\"");
 	}
 	
-	public void bulkAddStudents(int num) {
-		String name;
-		int id;
-		
-		for (int i = 0; i < num; i++) {
-			// prompt for name
-			System.out.println("Student " + (i + 1) + "/" + num + " name: ");
-			
-			// TODO: make sure they enter something
-			name = new Scanner(System.in).nextLine();
-			id = this.db.addStudent(name);
-			
-			System.out.println(name + " added (ID " + id + ")");
-		}
-		name = null;
-		//word = scanner.next();
-	}
-	
 	/*
 	public int setTest(String testName) {
 		String test;
@@ -96,49 +127,9 @@ public class TestInterfaceCli {
 	}
 	*/
 	
-	public void setResultsFor(int testID) {
-		int percent;
-		for (Integer id: this.db.getAllStudentIds()) {
-			// prompt for percentage
-			System.out.println("Result for " + this.db.getStudentName(id) + " (ID " + id + ") (%): ");
-			try {
-				percent = new Scanner(System.in).nextInt();
-			} catch (InputMismatchException e) {
-				percent = 0;
-			} finally {}
-			this.db.setResultOfStudent(id, testID, percent);
-		}
-	}
-	
-	public void addSet(String testName) {
-		int testID = this.db.addTest(testName, "true", 2, 2, 2014);
-		System.out.println(testID);
-		for (Integer id: this.db.getAllStudentIds()) {
-			this.db.setResultOfStudent(id, testID, 10);
-		}
-	}
-	
 	public void runTest() {
 		// initialise a database for holding student & result data
 		this.db = new TestDatabase();
-		
-		// add student
-		//int student1 = this.db.addStudent("Ben");
-		
-		// add test
-		//int test1 = this.db.addTest("English 1");
-
-		// result for test student doesn't have result for
-		//this.printTestResult(student1, test1);
-		
-		//this.db.printResultOf(student1, 0);
-		//p(this.db.IdOfStudent("Ben"));
-		
-		//this.addSet("Test test");
-		//this.db.printResultOf(2, 0);
-		//p(this.db.students.get(student1).getResult(2));
-		
-		//p(this.db.getResults(student1));
 		
 		this.bulkAddStudents(5);
 		int test1 = this.db.addTest("Computer Science 1", "true", 2, 2, 2014);
@@ -149,14 +140,5 @@ public class TestInterfaceCli {
 	public static void main(String[] args) {
 		TestInterfaceCli test = new TestInterfaceCli();
 		test.runTest();
-	}
-	
-	// System.out.println takes sooooooo long to type
-	public static void p(String s) {
-		System.out.println(s);
-	}
-	
-	public static void p(int s) {
-		System.out.println(s);
 	}
 }
