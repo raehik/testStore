924ff213df78f8d5874a93924e31efcb387c25af
Ben Orchard <thefirstmuffinman@gmail.com>
2014-09-21 09:35:32 +0100

    Store Date in test; take string rather than 3 ints
    The Java Date type allows us to compare dates and parse strings to dates
    ez. Also makes initialising a Test easier, since SimpleDateFormat can
    parse a string for a date and turn it into a Date object.

--- Test.java
+++ Test.java
@@ -1,17 +1,24 @@
 package testStore;
 
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class Test {
 	private String name, set;
-	// date variables for when the test was set
-	private int day, month, year;
+	public Date date;
 	
-	public Test(String name, String set, int day, int month, int year) {
+	public Test(String name, String set, String date) {
 		// remove trailing whitespace from name!
 		this.name = name;
 		this.set = set;
-		this.day = day;
-		this.month = month;
-		this.year = year;
+		
+		SimpleDateFormat simpleDate = new SimpleDateFormat("dd/MM/yy");
+		try {
+			this.date = simpleDate.parse(date);
+		} catch (ParseException e) {
+			// wat do???
+		}
 	}
 	
 	public String getName() {
@@ -20,10 +27,5 @@ public class Test {
 	
 	public String getSet() {
 		return this.set;
-	}
-	
-	// get date of test (when it was set)
-	public String getDate() {
-		return this.day + "/" + this.month + "/" + this.year;
 	}
 }
